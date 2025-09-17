d3984f4f90fe2685a6a772800ccdd05dfe6462d7
unknown <vansh.y.v.y@gmail.com>
2025-09-17 21:39:52 +0530

    Updated main and tested for the code

---
+++ utils/__init__.py
@@ -0,0 +1,1 @@
+from utils.video_utils import read_video, save_video
--- utils/video_utils.py
+++ utils/video_utils.py
@@ -1,4 +1,5 @@
 import cv2
+import os
 
 def read_video(video_path):
     cap = cv2.VideoCapture(video_path)
@@ -11,9 +12,19 @@ def read_video(video_path):
     cap.release()
     return frames
 
+
+
 def save_video(output_video_frames, output_video_path):
+    # Ensure folder exists
+    os.makedirs(os.path.dirname(output_video_path), exist_ok=True)
+
+    height, width = output_video_frames[0].shape[:2]
     fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-    out = cv2.VideoWriter(output_video_path, fourcc, 24, (output_video_frames[0].shape[1], output_video_frames[0].shape[0]))
+    out = cv2.VideoWriter(output_video_path, fourcc, 24, (width, height))
+
+    if not out.isOpened():
+        raise RuntimeError(f"Failed to open VideoWriter at {output_video_path}")
+
     for frame in output_video_frames:
         out.write(frame)
     out.release()
